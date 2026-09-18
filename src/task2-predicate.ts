7de55ca550b80c96483532f3403ccd09c56186fd
unknown <Administrator@NIR-N092.nir.center>
2026-09-18 15:17:00 +0500

    сделала задание 2

--- src/task2-predicate.ts
+++ src/task2-predicate.ts
@@ -11,12 +11,26 @@ export type Transaction = {
 // 2. Что у data есть поля id, amount, type
 // 3. Что id - это строка, amount - число, type - одна из двух строк
 export function isTransaction(data: unknown): data is Transaction {
-  // Напишите код здесь
+  if (typeof data !== "object" || data === null) {
+    return false;
+  }
+  const transaction = data as Partial<Transaction>;
+  return (
+    "id" in transaction &&
+    "amount" in transaction &&
+    "type" in transaction &&
+    typeof transaction.id === "string" &&
+    typeof transaction.amount === "number" &&
+    (transaction.type === "deposit" || transaction.type === "withdrawal")
+  );
 }
 
 // Функция обработки. 
 // Если data это транзакция - вернуть "Обработана транзакция на сумму <amount>"
 // Иначе вернуть "Неизвестные данные"
 export function processTransaction(data: unknown): string {
-  // Напишите код здесь, используя isTransaction
+  if (isTransaction(data)) {
+    return `Обработана транзакция на сумму ${data.amount}`;
+  }
+  return "Неизвестные данные";
 }
